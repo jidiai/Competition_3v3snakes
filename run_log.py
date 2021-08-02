891e4764954d3d3403ef376dc30a4037cc242f62
Yutongamber <yutongamber@yahoo.com>
2021-08-03 00:15:07 +0800

    update

--- run_log.py
+++ run_log.py
@@ -171,8 +171,8 @@ if __name__ == "__main__":
     render_mode = True
 
     parser = argparse.ArgumentParser()
-    parser.add_argument("--my_ai", default="random", help="rl/random/greedy")
-    parser.add_argument("--opponent", default="rl", help="rl/random/greedy")
+    parser.add_argument("--my_ai", default="random", help="rl/random")
+    parser.add_argument("--opponent", default="rl", help="rl/random")
     args = parser.parse_args()
 
     policy_list = [args.my_ai, args.opponent]
